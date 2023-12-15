91e77e12aa187654c425cc55e2ef7cf9fb89e8cf
22423MiguelG <113510733+22423MiguelG@users.noreply.github.com>
2023-12-15 01:55:54 +0000

    Initial commit

---
+++ src/players.js
@@ -0,0 +1,108 @@
+// players.js
+
+export const players = [
+    {
+      id: 1,
+      firstName: 'Lionel',
+      lastName: 'Messi',
+      age: 34,
+      club: 'Paris Saint-Germain',
+      trophies: ['10 La Liga titles', '4 UEFA Champions League titles', '7 Copa del Rey titles'],
+      nationality: 'Argentine',
+      bio: 'Lionel Messi, born on June 24, 1987, is an Argentine professional footballer who currently plays for Paris Saint-Germain and the Argentina national team. Messi is widely regarded as one of the greatest football players of all time. He spent the majority of his career at Barcelona, where he won numerous titles, including multiple La Liga and UEFA Champions League titles. In 2021, Messi joined Paris Saint-Germain, adding another chapter to his illustrious career. Known for his incredible dribbling, precise finishing, and vision on the field, Messi continues to leave an indelible mark on the world of football.'
+    },
+    {
+      id: 2,
+      firstName: 'Cristiano',
+      lastName: 'Ronaldo',
+      age: 37,
+      club: 'Manchester United',
+      trophies: ['3 Premier League titles', '5 UEFA Champions League titles', '1 UEFA European Championship'],
+      nationality: 'Portuguese',
+      bio: 'Cristiano Ronaldo, born on February 5, 1985, is a Portuguese professional footballer who currently plays for Manchester United and the Portugal national team. Ronaldo is widely known for his goal-scoring prowess, athleticism, and dedication to his craft. He has had a storied career, achieving success with clubs like Manchester United and Real Madrid. Ronaldo returned to Manchester United in 2021, aiming to continue his legacy as one of the greatest footballers in history.'
+    },
+    {
+      id: 3,
+      firstName: 'Neymar',
+      lastName: 'Jr.',
+      age: 30,
+      club: 'Paris Saint-Germain',
+      trophies: ['2 La Liga titles', '1 UEFA Champions League title', '1 Copa del Rey title'],
+      nationality: 'Brazilian',
+      bio: 'Neymar Jr., born on February 5, 1992, is a Brazilian professional footballer who plays as a forward for Paris Saint-Germain and the Brazil national team. Known for his skillful dribbling, flair, and goal-scoring ability, Neymar rose to prominence at a young age. He has won domestic league titles and continental honors, contributing significantly to his teams\' success. Neymar continues to be a key player for both club and country.'
+    },
+    {
+      id: 4,
+      firstName: 'Kylian',
+      lastName: 'Mbappe',
+      age: 23,
+      club: 'Paris Saint-Germain',
+      trophies: ['3 Ligue 1 titles', '1 FIFA World Cup'],
+      nationality: 'French',
+      bio: 'Kylian Mbappe, born on December 20, 1998, is a French professional footballer known for his explosive pace and goal-scoring ability. Mbappe gained global recognition for his performances with Paris Saint-Germain and the French national team. At a young age, he played a crucial role in France\'s victory in the 2018 FIFA World Cup. Mbappe\'s talent and potential have made him one of the most sought-after players in world football.'
+    },
+    {
+      id: 5,
+      firstName: 'Robert',
+      lastName: 'Lewandowski',
+      age: 33,
+      club: 'Bayern Munich',
+      trophies: ['7 Bundesliga titles', '1 UEFA Champions League title'],
+      nationality: 'Polish',
+      bio: 'Robert Lewandowski, born on August 21, 1988, is a Polish professional footballer who serves as the captain of the Poland national team and plays as a striker for Bayern Munich. Lewandowski is renowned for his goal-scoring prowess, finishing ability, and leadership on the field. He has consistently been one of the top goal-scorers in European football and played a pivotal role in Bayern Munich\'s success, including winning the UEFA Champions League.'
+    },
+    {
+      id: 6,
+      firstName: 'Mohamed',
+      lastName: 'Salah',
+      age: 30,
+      club: 'Liverpool',
+      trophies: ['1 Premier League title', '1 UEFA Champions League title'],
+      nationality: 'Egyptian',
+      bio: 'Mohamed Salah, born on June 15, 1992, is an Egyptian professional footballer who plays as a forward for Liverpool and the Egypt national team. Salah is known for his speed, agility, and clinical finishing in front of goal. He played a key role in Liverpool\'s success, contributing to their Premier League and UEFA Champions League triumphs. Salah has earned numerous individual accolades for his outstanding performances.'
+    },
+    {
+      id: 7,
+      firstName: 'Kevin',
+      lastName: 'De Bruyne',
+      age: 31,
+      club: 'Manchester City',
+      trophies: ['4 Premier League titles', '1 UEFA Champions League title'],
+      nationality: 'Belgian',
+      bio: 'Kevin De Bruyne, born on June 28, 1991, is a Belgian professional footballer known for his vision, passing ability, and versatility on the field. De Bruyne plays as a midfielder for Manchester City and the Belgium national team. He has been instrumental in Manchester City\'s success, helping them secure multiple Premier League titles and reaching new heights in European competitions. De Bruyne is considered one of the best midfielders in the world.'
+    },
+    {
+      id: 8,
+      firstName: 'Sergio',
+      lastName: 'Ramos',
+      age: 36,
+      club: 'Paris Saint-Germain',
+      trophies: ['5 La Liga titles', '4 UEFA Champions League titles', '1 FIFA World Cup'],
+      nationality: 'Spanish',
+      bio: 'Sergio Ramos, born on March 30, 1986, is a Spanish professional footballer who plays as a central defender. Ramos has had a illustrious career, spending the majority of it at Real Madrid, where he became one of the club\'s all-time greats. Known for his defensive skills, leadership, and goal-scoring ability, Ramos has won numerous domestic and international titles, including multiple UEFA Champions League trophies. In 2021, Ramos joined Paris Saint-Germain.'
+    },
+    {
+      id: 9,
+      firstName: 'Virgil',
+      lastName: 'van Dijk',
+      age: 30,
+      club: 'Liverpool',
+      trophies: ['1 Premier League title', '1 UEFA Champions League title'],
+      nationality: 'Dutch',
+      bio: 'Virgil van Dijk, born on July 8, 1991, is a Dutch professional footballer who plays as a central defender for Liverpool and the Netherlands national team. Van Dijk is known for his strength, composure, and ability to organize the defense. He played a pivotal role in Liverpool\'s defensive solidity, contributing to their success in winning the Premier League and UEFA Champions League. Van Dijk has established himself as one of the best defenders in the world.'
+    },
+    {
+      id: 10,
+      firstName: 'Joshua',
+      lastName: 'Kimmich',
+      age: 27,
+      club: 'Bayern Munich',
+      trophies: ['7 Bundesliga titles', '1 UEFA Champions League title'],
+      nationality: 'German',
+      bio: 'Joshua Kimmich, born on February 8, 1995, is a German professional footballer who plays as a midfielder for Bayern Munich and the Germany national team. Kimmich is known for his versatility, technical ability, and leadership on the field. He has been a key figure in Bayern Munich\'s midfield, contributing to their domestic and international success. Kimmich is regarded as one of the top midfielders in world football.'
+    },
+  ];
+  
+  export default players;
+  
+  
